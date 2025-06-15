ed035dc7dca271724a8762d1426fafec09361822
skryu89 <mtjapwd@gmail.com>
2025-06-16 02:23:47 +0900

    Add new theme files

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function my_enqueue_styles() {
+  wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
+  wp_enqueue_style('style', get_stylesheet_uri(), array('ress'), false, 'all');
+}
+add_action('wp_enqueue_scripts', 'my_enqueue_styles');
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang=”ja”>
+<head>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <header>
+  </header>
