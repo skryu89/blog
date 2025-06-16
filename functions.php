237dcdd469d34b0735d260725535cf7700f2bdcc
skryu89 <mtjapwd@gmail.com>
2025-06-17 01:11:31 +0900

    commit test

--- functions.php
+++ functions.php
@@ -5,3 +5,5 @@ function my_enqueue_styles() {
   wp_enqueue_style('style', get_stylesheet_uri(), array('ress'), false, 'all');
 }
 add_action('wp_enqueue_scripts', 'my_enqueue_styles');
+
+add_theme_support('post-thumbnails');
--- index.php
+++ index.php
@@ -1,1 +1,39 @@
 <?php get_header(); ?>
+
+<div id="container" class="wrapper">
+  <main>
+    <?php if(have_posts()): ?>
+      <?php while(have_posts()):the_post(); ?>
+        <?php
+          $cat = get_the_category();
+          $catname = $cat[0]->cat_name;
+        ?>
+        <article>
+          <h2 class="article-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </h2>
+          <ul class="meta">
+            <li><?php the_time('Y/m/d'); ?></li>
+            <li><?php echo $catname; ?></li>
+          </ul>
+          <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+          <div class="text">
+            <?php
+              if (mb_strlen(strip_tags(get_the_content()), 'UTF-8') > 80) {
+                $content = mb_substr(strip_tags(get_the_content()), 0, 80, 'UTF-8');
+                echo $content . '…';
+              } else {
+                echo strip_tags(get_the_content());
+              }
+            ?>
+          </div>
+          <div class="readmore"><a href="<?php the_permalink(); ?>">READ MORE</a></div>
+        </article>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </main>
+</div>
+
+
+
+<?php get_footer(); ?>
